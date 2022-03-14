4659ead9561ad6a6a001cfe846b0d50321a2be33
Juan Escalona <85139773+Juanes8@users.noreply.github.com>
2022-03-13 21:40:47 -0600

    Upload query

---
+++ src/tweets_SF.sql
@@ -0,0 +1,1 @@
+SELECT count (*) FROM `dpa-2022-itam.prueba_twits.historical_tweets` where author_location = 'San Francisco'
---
+++ src/tweets_feb_2021.sql
@@ -0,0 +1,1 @@
+SELECT count (*) FROM `dpa-2022-itam.prueba_twits.historical_tweets` where created_at between '2021-02-08' and '2021-02-09'
